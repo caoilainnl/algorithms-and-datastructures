1ff051b52113d888cfb842161af01c18979d048e
caoilainnl <caoilainn.lynch@proton.me>
2023-10-31 16:55:22 -0600

    merge sort

---
+++ merge-sort.py
@@ -0,0 +1,37 @@
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    # Divide the array into two halves
+    mid = len(arr) // 2
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
+
+    # Merge the sorted halves
+    return merge(left_half, right_half)
+
+
+def merge(left, right):
+    merged = []
+    i = j = 0
+
+    # Merge while comparing elements of left and right
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+
+    # Append any remaining elements
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+
+    return merged
+
+# Example usage
+array = [38, 27, 43, 3, 9, 82, 10]
+sorted_array = merge_sort(array)
+print(sorted_array)
